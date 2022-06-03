2b0bee0f7c2727e43baa0ad449571007950ea792
Keshav Aggarwal <73278987+keshavagg22@users.noreply.github.com>
2022-06-04 00:41:48 +0530

    Added solution - LeetHub

---
+++ 2. Print GeeksForGeeks - GFG/2.-print-geeks-for-geeks.cpp
@@ -0,0 +1,31 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+void print(){
+    
+    // Prints Hello World
+    //cout<<"Hello World\n";
+    
+    // Print GeeksforGeeks
+    cout<<"GeeksForGeeks\n";
+}
+
+// { Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        print();
+    }
+    
+}  // } Driver Code Ends
