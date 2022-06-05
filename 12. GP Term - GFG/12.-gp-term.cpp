528ef91cef02efbd3b07937100db24f342224a8e
Keshav Aggarwal <73278987+keshavagg22@users.noreply.github.com>
2022-06-06 01:35:27 +0530

    Added solution - LeetHub

---
+++ 12. GP Term - GFG/12.-gp-term.cpp
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+void utility(int a, int r, int n){
+    //Complete the code below
+    int ans = a*int(pow(r,n-1));
+    //Complete the code above
+    
+    //The line below prints the output. Don't change it!
+
+    cout << ans << endl;
+}
+
+// { Driver Code Starts.
+    
+int main() {
+	
+	int t; cin>>t;
+	while(t-- > 0) {
+        int a, r, n;
+        cin>>a>>r>>n;
+        utility(a, r, n);
+    }
+	
+}  // } Driver Code Ends
