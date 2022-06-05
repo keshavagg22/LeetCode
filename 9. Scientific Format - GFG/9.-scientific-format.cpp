eb4661564602340f7b481c1a72c21e0d4d7b2b8a
Keshav Aggarwal <73278987+keshavagg22@users.noreply.github.com>
2022-06-06 00:41:16 +0530

    Added solution - LeetHub

---
+++ 9. Scientific Format - GFG/9.-scientific-format.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+void scientific(double a)
+{
+    //code here
+    cout<<std::setprecision(4);
+    cout<<std::scientific;
+    cout<<a;
+   
+}
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    double a;
+	    cin>>a; 
+	    scientific(a);
+	    cout<<endl;
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
