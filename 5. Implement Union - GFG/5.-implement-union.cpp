515f8f0fec5175ee041db5efc315bdbb0e44db98
Keshav Aggarwal <73278987+keshavagg22@users.noreply.github.com>
2022-06-09 15:35:41 +0530

    Added solution - LeetHub

---
+++ 5. Implement Union - GFG/5.-implement-union.cpp
@@ -0,0 +1,27 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+/*create binary tree struct node*/
+// your code here
+struct NODE{
+  double data;
+  union{
+      NODE *left;
+      NODE *right;
+  };
+};
+
+// { Driver Code Starts.
+#include <bits/stdc++.h>
+int main() {
+	//code
+	
+	struct NODE ll;
+	printf("%d", 1);
+	return 0;
+}  // } Driver Code Ends
