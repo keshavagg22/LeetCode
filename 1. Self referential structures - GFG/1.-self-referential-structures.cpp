2b5fbdcb6eaad159635853051603eda81f1d2e3b
Keshav Aggarwal <73278987+keshavagg22@users.noreply.github.com>
2022-06-09 15:24:48 +0530

    Added solution - LeetHub

---
+++ 1. Self referential structures - GFG/1.-self-referential-structures.cpp
@@ -0,0 +1,26 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+/*create linked list struct node*/
+// your code here
+struct node{
+    int data;
+    struct{
+        node *next;
+    };
+};
+
+// { Driver Code Starts.
+#include <bits/stdc++.h>
+int main() {
+	//code
+	
+	struct node ll;
+	printf("%d", 1);
+	return 0;
+}  // } Driver Code Ends
