12582c1edec936eacd9a80721e2e9498c3884d8f
Keshav Aggarwal <73278987+keshavagg22@users.noreply.github.com>
2022-06-15 16:56:28 +0530

    Added solution - LeetHub

---
+++ 9. Gray to Binary equivalent - GFG/9.-gray-to-binary-equivalent.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // function to convert a given Gray equivalent n to Binary equivalent.
+    int grayToBinary(int n)
+    {
+        int x = 0;
+        // Your code here
+        while(n){
+            x = x^n;
+            n = n>>1;
+        }
+        return x;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t,n;
+    cin>>t;//testcases
+    while(t--)
+    {
+        cin>>n;//initializing n
+        
+        Solution ob;
+        //calling function grayToBinary()
+       cout<< ob.grayToBinary(n)<<endl;
+    }
+}
+  // } Driver Code Ends
