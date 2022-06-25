e62a18a3d2d07c4eabfb1ec24e73cb165bb2af2f
Keshav Aggarwal <73278987+keshavagg22@users.noreply.github.com>
2022-06-25 16:10:10 +0530

    Added solution - LeetHub

---
+++ 10. Keypad typing - GFG/10.-keypad-typing.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string printNumber(string , int);
+
+// User function pasted here
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s,temp;
+		cin>>s;
+		int i,len=s.length();
+		
+		cout << printNumber(s,len) << "\n";
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+//User function template for C++
+
+
+//Function to find matching decimal representation of a string as on the keypad.
+string printNumber(string s, int n) 
+{
+    //code here
+    string ans;
+    string numbers = "22233344455566677778889999";
+    for (int i=0;i<n;i++){
+        ans += numbers[s[i]-'a'];
+    }
+    
+    return ans;
+}
