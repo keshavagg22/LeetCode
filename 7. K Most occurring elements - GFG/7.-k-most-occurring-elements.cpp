68cc6cd058130cc30c488b364fd8bd9c31973972
Keshav Aggarwal <73278987+keshavagg22@users.noreply.github.com>
2022-07-09 14:38:05 +0530

    Added solution - LeetHub

---
+++ 7. K Most occurring elements - GFG/7.-k-most-occurring-elements.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+// Function to print the k numbers with most occurrences 
+
+class Solution
+{
+    public:
+    //Function to return the sum of frequencies of k numbers
+    //with most occurrences in an array.
+    int kMostFrequent(int arr[], int n, int k) 
+    { 
+    	// Your code here
+    	unordered_map<int, int> mp;
+    	for(int i = 0; i < n; i++){
+    	    mp[arr[i]]++;
+    	}
+    	int sum=0;
+    	priority_queue<int> pq;
+    	for(auto itr:mp){
+    	   pq.push(itr.second);
+    	}
+    	
+    	while(k--){
+    	    sum+=pq.top();
+    	    pq.pop();
+    	}
+    	
+    	return sum;
+    } 
+};
+
+// { Driver Code Starts.
+// Driver program to test above 
+int main() 
+{
+	int t;
+    cin >> t;
+
+    while(t--){
+        int n, k;
+        cin >> n;
+        int arr[n];
+        for(int i = 0; i < n; i++){
+            cin >> arr[i];
+        }
+        cin >> k;
+        Solution ob;
+        cout << ob.kMostFrequent(arr, n, k) << endl; 
+    }
+
+	return 0; 
+} 
+  // } Driver Code Ends
