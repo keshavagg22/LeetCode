2c2d34ec783ff1a21b385b2c1184d785cf034b82
Keshav Aggarwal <73278987+keshavagg22@users.noreply.github.com>
2023-07-21 17:33:06 +0530

    Added solution - LeetHub

---
+++ Immediate Smaller Element - GFG/immediate-smaller-element.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	void immediateSmaller(vector<int>&a, int n) {
+	    //  code here
+	    for(int i = 0; i < n - 1; i++){
+	        if(a[i+1] < a[i]){
+	            a[i] = a[i+1];
+	        }
+	        else{
+	            a[i] = -1;
+	        }
+	    }
+	    a[n-1] = -1;
+	    return;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, l, r;
+        cin >> n;
+        vector<int>arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.immediateSmaller(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
